12dda950785a07ca1647553bd7e9e3cd57921268
uridabomb <uriavron@gmail.com>
2019-04-03 08:03:06 +0300

    Adds answers

--- assignments/01/hw1/answers.py
+++ assignments/01/hw1/answers.py
@@ -12,12 +12,9 @@ part2_q1 = r"""
 **Your answer:**
 
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+
+Increasing `k` increases the generalisation until a cerain point, we can see in the graph that after `k = 3` the accuracy starts to decline.
+this happens because we let more samples that might be farther apart and of the wrong class. to emphasize this point, let's think of what happens when `k == N` (`N` is size of dataset) in that case we just return the majority label in the dataset.
 
 """
 # ==============
@@ -32,26 +29,26 @@ the selection of any $\Delta > 0$ is arbitrary because this effects
 only the scale of the weights and not the quality of the linear seperation
 as we learn, the weights change to optimaly seperate the data.
 if the optimal solution for some $\Delta > 0$ is a weight matrix $\mat{W}$
-than the optimal solution we learn for a margin of $\alpha\Delta$ will be $\alpha\mat{M}$
+ than the optimal solution we learn for a margin of $\alpha\Delta$ will be $\alpha\mat{M}$
 
 """
 
 part3_q2 = r"""
 **Your answer:**
 
-1. form the images we can see every separator has learned
+1. From the images we can see that every separator has learned
  which pixels are most important to tell this class apart from others and what their values should be.
- for instance we see in all figures an area that is usually part of the figure (middle bottom and a bit right) but still gives almost no information about the classification
- this is not necessarily how we tell numbers apart for instance there is a sample of a 5 with a small bottom part that is mistaken for 6 because usually 5s have a large round open bottom
- we can also tell numbers apart by length relations
-2. 
+ For instance, we see in all figures an area that is usually part of the figure (middle bottom and a bit right) but still gives almost no information about the classification
+ this is not necessarily how we tell numbers apart. For instance, there is a sample of a 5 with a small bottom part that is mistaken for 6 because usually 5s have a large round open bottom.
+ We can also tell numbers apart by length relations.
+2. This is different from KNN as the KNN classifies by taking into consideration all the pixels equally.
 
 """
 
 part3_q3 = r"""
 **Your answer:**
 
-1. good, we see a constant decrease in the loss without any zigzags that occur with a learning rate too high.
+1. Good, we see a constant decrease in the loss without any zigzags that occur with a learning rate too high.
 yet we do manage to learn and reach what seems to be a plato which indicates that we cannot learn a lot more
 2. Slightly overfitted to the training set - as we see that the train loss is below the validation loss yet the accuracy on the train set is higher than the validation set
 
@@ -65,26 +62,17 @@ yet we do manage to learn and reach what seems to be a plato which indicates tha
 part4_q1 = r"""
 **Your answer:**
 
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+The ideal pattern is a constant line at y=0. We can say that our residual graph seems pretty similar to that, hence our model fitted relatively okay.
+The CV graph looks more packed around 0 than the top-5 graph, so we can infer that after CV the fitting is better.
 
 """
 
 part4_q2 = r"""
 **Your answer:**
 
+Using the logspace allows us to explore values accross different orders of magnitude without sampling huge amounts of lambdas.
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+The model is fitted `k_folds * len(degree_range) * len(lambda_range)` number of times (not including the final fit).
 
 """
 

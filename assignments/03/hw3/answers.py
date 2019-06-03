2d8c7292dbf89ae61a79f0f009aaac12ce9694d5
uridabomb <uriavron@gmail.com>
2019-06-03 12:15:39 +0300

    Adds assignment 03 files

---
+++ assignments/03/hw3/answers.py
@@ -0,0 +1,304 @@
+r"""
+Use this module to write your answers to the questions in the notebook.
+
+Note: Inside the answer strings you can use Markdown format and also LaTeX
+math (delimited with $$).
+"""
+
+# ==============
+# Part 2 answers
+
+
+def part2_overfit_hp():
+    wstd, lr, reg = 0, 0, 0
+    # TODO: Tweak the hyperparameters until you overfit the small dataset.
+    # ====== YOUR CODE: ======
+    raise NotImplementedError()
+    # ========================
+    return dict(wstd=wstd, lr=lr, reg=reg)
+
+
+def part2_optim_hp():
+    wstd, lr_vanilla, lr_momentum, lr_rmsprop, reg, = 0, 0, 0, 0, 0
+
+    # TODO: Tweak the hyperparameters to get the best results you can.
+    # You may want to use different learning rates for each optimizer.
+    # ====== YOUR CODE: ======
+    raise NotImplementedError()
+    # ========================
+    return dict(wstd=wstd, lr_vanilla=lr_vanilla, lr_momentum=lr_momentum,
+                lr_rmsprop=lr_rmsprop, reg=reg)
+
+
+def part2_dropout_hp():
+    wstd, lr, = 0, 0
+    # TODO: Tweak the hyperparameters to get the model to overfit without
+    # dropout.
+    # ====== YOUR CODE: ======
+    raise NotImplementedError()
+    # ========================
+    return dict(wstd=wstd, lr=lr)
+
+
+part2_q1 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part2_q2 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+# ==============
+
+# ==============
+# Part 3 answers
+
+part3_q1 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part3_q2 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part3_q3 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+
+part3_q4 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+# ==============
+# Part 4 answers
+
+
+def part4_generation_params():
+    start_seq = ""
+    temperature = .0001
+    # TODO: Tweak the parameters to generate a literary masterpiece.
+    # ====== YOUR CODE: ======
+    raise NotImplementedError()
+    # ========================
+    return start_seq, temperature
+
+
+part4_q1 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part4_q2 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part4_q3 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part4_q4 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+# ==============
+
+
+# ==============
+# Part 5 answers
+
+PART5_CUSTOM_DATA_URL = None
+
+
+def part5_vae_hyperparams():
+    hypers = dict(
+        batch_size=0,
+        h_dim=0, z_dim=0, x_sigma2=0,
+        learn_rate=0.0, betas=(0.0, 0.0),
+    )
+    # TODO: Tweak the hyperparameters to generate a former president.
+    # ====== YOUR CODE: ======
+    raise NotImplementedError()
+    # ========================
+    return hypers
+
+
+part5_q1 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part5_q2 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+# ==============
+
+# ==============
+# Part 6 answers
+
+PART6_CUSTOM_DATA_URL = None
+
+
+def part6_gan_hyperparams():
+    hypers = dict(
+        batch_size=0, z_dim=0,
+        data_label=0, label_noise=0.0,
+        discriminator_optimizer=dict(
+            type='',  # Any name in nn.optim like SGD, Adam
+            lr=0.0,
+        ),
+        generator_optimizer=dict(
+            type='',  # Any name in nn.optim like SGD, Adam
+            lr=0.0,
+        ),
+    )
+    # TODO: Tweak the hyperparameters to train your GAN.
+    # ====== YOUR CODE: ======
+    raise NotImplementedError()
+    # ========================
+    return hypers
+
+
+part6_q1 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part6_q2 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part6_q3 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""

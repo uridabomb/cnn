a07b67c56ec9bba2f93180387d3fd4af45cf9d2f
uridabomb <uriavron@gmail.com>
2019-06-06 09:13:41 +0300

    Adds script for running experiment1 in part 3

---
+++ assignments/03/part3_experiment1.sh
@@ -0,0 +1,29 @@
+python -m hw3.experiments run-exp -n exp1_1_K32_L2 -K 32 -L 2 -P 2 -H 64
+python -m hw3.experiments run-exp -n exp1_1_K32_L4 -K 32 -L 4 -P 2 -H 64
+python -m hw3.experiments run-exp -n exp1_1_K32_L8 -K 32 -L 8 -P 2 -H 64
+python -m hw3.experiments run-exp -n exp1_1_K32_L16 -K 32 -L 16 -P 2 -H 64
+
+python -m hw3.experiments run-exp -n exp1_1_K64_L2 -K 64 -L 2 -P 2 -H 64
+python -m hw3.experiments run-exp -n exp1_1_K32_L4 -K 64 -L 4 -P 2 -H 64
+python -m hw3.experiments run-exp -n exp1_1_K32_L8 -K 64 -L 8 -P 2 -H 64
+python -m hw3.experiments run-exp -n exp1_1_K32_L16 -K 64 -L 16 -P 2 -H 64
+
+python -m hw3.experiments run-exp -n exp1_2_L2_K32 -K 32 -L 2 -P 2 -H 64
+python -m hw3.experiments run-exp -n exp1_2_L2_K64 -K 64 -L 2 -P 2 -H 64
+python -m hw3.experiments run-exp -n exp1_2_L2_K128 -K 128 -L 2 -P 2 -H 64
+python -m hw3.experiments run-exp -n exp1_2_L2_K256 -K 256 -L 2 -P 2 -H 64
+
+python -m hw3.experiments run-exp -n exp1_2_L4_K32 -K 32 -L 4 -P 2 -H 64
+python -m hw3.experiments run-exp -n exp1_2_L4_K64 -K 64 -L 4 -P 2 -H 64
+python -m hw3.experiments run-exp -n exp1_2_L4_K128 -K 128 -L 4 -P 2 -H 64
+python -m hw3.experiments run-exp -n exp1_2_L4_K256 -K 256 -L 4 -P 2 -H 64
+
+python -m hw3.experiments run-exp -n exp1_2_L8_K32 -K 32 -L 8 -P 2 -H 64
+python -m hw3.experiments run-exp -n exp1_2_L8_K64 -K 64 -L 8 -P 2 -H 64
+python -m hw3.experiments run-exp -n exp1_2_L8_K128 -K 128 -L 8 -P 2 -H 64
+python -m hw3.experiments run-exp -n exp1_2_L8_K256 -K 256 -L 8 -P 2 -H 64
+
+python -m hw3.experiments run-exp -n exp1_3_L1_K64-128-256 -K 64 128 256 -L 1 -P 2 -H 64
+python -m hw3.experiments run-exp -n exp1_3_L2_K64-128-256 -K 64 128 256 -L 2 -P 2 -H 64
+python -m hw3.experiments run-exp -n exp1_3_L3_K64-128-256 -K 64 128 256 -L 3 -P 2 -H 64
+python -m hw3.experiments run-exp -n exp1_3_L4_K64-128-256 -K 64 128 256 -L 4 -P 2 -H 64

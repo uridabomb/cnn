4d9cde262658d2cab3a8b118eb2a088d5d225754
uridabomb <uriavron@gmail.com>
2019-06-16 10:19:29 +0300

    Adds exps

--- assignments/03/part3_experiment2.sh
+++ assignments/03/part3_experiment2.sh
@@ -2,3 +2,8 @@ python -m hw3.experiments run-exp -n exp2_L1_K64-128-256-512 -K 64 128 256 512 -
 python -m hw3.experiments run-exp -n exp2_L2_K64-128-256-512 -K 64 128 256 512 -L 2 -P 2 -H 64 64 --ycn
 python -m hw3.experiments run-exp -n exp2_L3_K64-128-256-512 -K 64 128 256 512 -L 3 -P 3 -H 64 64 --ycn
 python -m hw3.experiments run-exp -n exp2_L4_K64-128-256-512 -K 64 128 256 512 -L 4 -P 4 -H 64 64 --ycn
+
+python -m hw3.experiments run-exp -n exp2_L1_K64-128-256 -K 64 128 256 -L 1 -P 1 -H 64 64 --ycn
+python -m hw3.experiments run-exp -n exp2_L2_K64-128-256 -K 64 128 256 -L 2 -P 2 -H 64 64 --ycn
+python -m hw3.experiments run-exp -n exp2_L3_K64-128-256 -K 64 128 256 -L 3 -P 3 -H 64 64 --ycn
+python -m hw3.experiments run-exp -n exp2_L4_K64-128-256 -K 64 128 256 -L 4 -P 4 -H 64 64 --ycn

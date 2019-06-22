3c88ea1b9aed0ab0fe0c8c42b966f734869ba55b
shgoren <34242458+shgoren@users.noreply.github.com>
2019-06-22 20:40:02 +0300

    answers recovered

--- assignments/03/hw3/answers.py
+++ assignments/03/hw3/answers.py
@@ -63,55 +63,46 @@ part2_q2 = r"""
 
 part3_q1 = r"""
 **Your answer:**
-
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+Explain the effect of depth on the accuracy. What depth produces the best results and why do you think that's the case?
+Were there values of L for which the network wasn't trainable? what causes this? Suggest two things which may be done to resolve it at least partially.
+As the depth gets bigger, the accuracy get smaller. As we can see, the best results are for L=2,4. L=8 has a bit lower 
+accuracy. For L=16 the network was not trainable. A reasonable explaination for those results is 
+the vanishing/exploding gradient phenomena.
+There are several things that may be done to resolve this problem. Among them:
+1. Add batch normalizations, which will make the gradients to be more stable.
+2. Use skip-connections, which will make the network to do less multiplications along the depth, and the gradient will 
+get bigger/smaller in a lower rate.
 
 """
 
 part3_q2 = r"""
 **Your answer:**
-
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
-
+As the best K vary for each L=2,4,8, we may analyze that there is no K that is better to use than the rest.
+In comparison to experiment 1.1, increase in K does not decrease the accuracy, as opposed to increase in L
+(from some threshold) in experminent. 
+Moreover, in experiment 1.2, there is no value of K which yields an untrainable
+network.
 """
 
 part3_q3 = r"""
 **Your answer:**
-
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
-
+The best results are for L=2, as the network's depth is okay, while L=4 yielded untrainable network as the network is
+too deep. 
+Those results fit nicely with our prior experiments, as we saw there is no best K and the network should not 
+be too deep or too shallow.
 """
 
 
 part3_q4 = r"""
 **Your answer:**
-
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
-
+We added batch normalization and dropout layers, in order to make the network be more trainable for more epochs,
+and to ba able to handle larger depths. Moreover, we replaced the linear layers with avarage pooling layer
+with output size (1, 1).
+L3_K64-128-256 has the best accuracy. L4_K64-128-256-512 has the worst result, 
+as even with normalization this network is too deep. 
+In comparison to experiment 1, we can see that the early-stopping mechainsm stopped the training after much more epochs, 
+and the test accuracy correlates much better with the train accuracy (they increase in a much closer rate).
+Moreover, the best final test accuracy improved significantly.
 """
 
 # ==============
@@ -123,60 +114,40 @@ def part4_generation_params():
     temperature = .0001
     # TODO: Tweak the parameters to generate a literary masterpiece.
     # ====== YOUR CODE: ======
-    raise NotImplementedError()
+    start_seq = "KING RAN:"
+    temperature = .1
     # ========================
     return start_seq, temperature
 
 
 part4_q1 = r"""
 **Your answer:**
-
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
-
+for 2 main reasons:
+- characters that are far from each other are less semantically connected or relevant to each other. 
+splitting to sequences prevents the loss from being effected by far, hence less relevant chars.
+- splitting to shorter sequences sequences speeds up the training - each batch takes less time to calaulate
+ and propogate changes to the model.
 """
 
 part4_q2 = r"""
 **Your answer:**
-
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+- the method for the model to "remember" more than sequence length is with the "hidden state" of the GRU.
 
 """
 
 part4_q3 = r"""
 **Your answer:**
-
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+unlike other learning tasks, here the order of samples is important. as we mentioned, past sequences influence the hidden state.
+hence -  it's important to feed them in correct order and not shuffle so that the "hidden state" represents the context accuratly..
 
 """
 
 part4_q4 = r"""
 **Your answer:**
-
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+- while sampling, we want out text to look structured and prevent mistakes and "random feeling" in the text.
+this is why we lower the temperature and "pushing" the more likely outputs to being even more likely.
+- very high temperature makes the chracter distribution very "soft": all characters have similar probability, and text looks more random.
+- very low temperature makes the probability distribution more "steep" -the largest logit is pushed close to 1, and the sampling becomes very conservative and non-diverse. similar sequences are likely to be generated many times. 
 
 """
 # ==============
